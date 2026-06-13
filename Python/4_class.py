e19dfe372677deecb8c0f4400ba505bf4e13e880
Mohammed Jameal <techprompt1031@gmail.com>
2026-06-13 16:05:49 +0530

    class

---
+++ Python/4_class.py
@@ -0,0 +1,17 @@
+class Car:
+    def __init__(self,brand,colour): #method to initialize object attributes
+        print(self)
+        self.brand = brand #object attribute
+        self.colour = colour
+
+    def start_engine(self):
+        return f"{self.brand} engine started."
+    
+
+car1 = Car("Toyota","Red") #object creation
+print(car1.start_engine())
+print(car1.__dict__)
+print(getattr(car1,"brand"))
+
+car2 = Car("Honda","Blue")
+print(car2.start_engine())
